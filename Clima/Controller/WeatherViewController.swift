434aec97ee0b530a86abf1705c751efdcca5fc63
Sal562 <salmanamer@gmail.com>
2020-11-02 20:09:51 +0700

    added textFieldDidEndEditing
    added textFieldDidEndEditing

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
-
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
@@ -17,15 +17,45 @@ class WeatherViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //text field should report back to view controller ..ie types return
+        searchTextField.delegate = self
     }
-
+    
     //MARK: Actions
     
     @IBAction func searchBtnWasPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
         print(searchTextField.text!)
+        
     }
     
+    // return btn pressed via delegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //dismiss keyboard
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    // textfield should editiing - Make sure the user entered text - Data Validation
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != ""  {
+            return true
+        } else {
+            textField.placeholder = "Dont forget to type the words dork"
+            return false
+        }
+    }
+    
+    // delegate method to clear search field after end editiing
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        // use searchfield.text to get weather for that city
+        
+        //clear textfield after typing
+        searchTextField.text = ""
+    }
     
 }
 

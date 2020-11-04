116dbc0d81e78aaf0e0b0d15c5ffb119895097e9
Sal562 <salmanamer@gmail.com>
2020-11-04 20:53:25 +0700

    setup trailing closure for data
    setup trailing closure for data

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -18,8 +18,8 @@ struct WeatherManger {
     let weatherURL = "https://api.openweathermap.org/data/2.5/find?appid=b687b8bdcf2dd6f944eb5e451446e887&units=metric"
     
     func fetchWeather(cityName: String) {
-            let urlString = "\(weatherURL)&q=\(cityName)"
-            performRequest(urlString: urlString)
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
     }
     
     //perform request     //Networking Steps
@@ -27,28 +27,38 @@ struct WeatherManger {
     func performRequest(urlString: String) {
         
         //1 Create URL
-            if let url = URL(string: urlString) {
-        //2 CreateURLSession
+        if let url = URL(string: urlString) {
+            //2 CreateURLSession
             let session = URLSession(configuration: .default)
-        //3 Give the session a Task
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error: ))
-        //4 Start the task
+            
+            //3 Give the session a Task ==OLD METHOD
+            // let task = session.dataTask(with: url, completionHandler: handle(data:response:error: ))
+            
+            //3.5 - IMPROVED Using Closure
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+//                    let dataString = String(data: safeData, encoding: .utf8)
+//                    print(dataString)
+                    
+                    //DONT PRINT. Parse Jason DATA to be readable
+                
+                    parseJSON(weatherData: safeData)
+                    
+                }
+            }
+            
+            //4 Start the task
             task.resume()
             
         }
     }
     
-    //deal with handler
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
-        }
+    func parseJSON(weatherData: Data) {
         
     }
     

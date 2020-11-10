71ffd3dcc4a0b53cac5ce8442cb20bcf9f2412f7
Krupali Patel <>
2020-11-09 20:54:43 -0500

    Added UITextField delegate

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,43 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
-
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var cityLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
-
-
+    
+    @IBAction func searchClicked(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text ?? "")
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text ?? "")
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if(searchTextField.text != ""){
+            return true
+        }
+        searchTextField.placeholder = "enter value"
+        return false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //Fetch value of entered text
+        //Fetch weather of the city
+        searchTextField.placeholder = "Search"
+        searchTextField.text = ""
+    }
 }
 
